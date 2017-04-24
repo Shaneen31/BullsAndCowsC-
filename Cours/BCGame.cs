c57f39cb0ea591c17f8ee77d890804f06f43f06b
Shaneen31 <Shaneen31@users.noreply.github.com>
2017-04-24 20:05:14 +0200

    First conversion of Bulls and Cows C++ version

---
+++ Cours/BCGame.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Generic;
+
+namespace Cours
+{
+    public class BullsCowsGame
+    {
+        private string _hiddenWord;
+        private int _currentTry;
+        private bool _isWon;
+        private int _bulls;
+        private int _cows;
+        
+        public enum EGuessStatus
+        {
+            Invalid,
+            Ok,
+            WrongLengh,
+            NotLowerCase,
+            NotIsogram
+        }
+
+        /************************************************************
+            Constructor
+        ************************************************************/
+        public BullsCowsGame()
+        {
+            Reset();
+        }
+
+        /************************************************************
+            Getters
+        ************************************************************/
+        public int GetHiddenWordLenght => _hiddenWord.Length;
+
+        public int GetCurrentTry => _currentTry;
+
+        // TODO Add multiple value according to HiddenWord Lenght
+        public int GetMaxTries => _hiddenWord.Length + 1;
+
+        public int GetBullsCount => _bulls;
+
+        public int GetConwsCount => _cows;
+
+        public bool IsGameWon => _isWon;
+
+        /************************************************************
+            Private Methods
+        ************************************************************/
+        public void Reset()
+        {
+            _hiddenWord = "planet";
+            _currentTry = 1;
+            _isWon = false;
+        }
+
+        public EGuessStatus ValidateGuess(string guess)
+        {
+            if (guess.Length != _hiddenWord.Length)
+            {
+                return EGuessStatus.WrongLengh;
+            }
+            else if (!IsLowerCase(guess))
+            {
+                return EGuessStatus.NotLowerCase;
+            }
+            else if (!IsIsogram(guess))
+            {
+                return EGuessStatus.NotIsogram;
+            }
+            else
+            {
+                return EGuessStatus.Ok;
+            }
+        }
+
+        // Check if player guess is lower case
+        bool IsLowerCase(string guess)
+        {
+            foreach (char letter in guess)
+            {
+                if (!char.IsLower(letter))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // Check if player guess is an isogram
+        bool IsIsogram(string guess)
+        {
+            if (guess.Length == 1)
+            {
+                return true;
+            }
+
+            List<char>letterList = new List<char>();
+
+            foreach (char letter in guess)
+            {
+                if (letterList.Contains(letter))
+                {
+                    return false;
+                }
+                else
+                {
+                    letterList.Add(letter);
+                }
+            }
+            return true;
+        }
+
+        // TODO Count Bulls & Cows in player's Guess
+        public void SubmitGuess(string guess)
+        {
+            _bulls = 0;
+            _cows = 0;
+
+            for (int i = 0; i < GetHiddenWordLenght; i++)
+            {
+                for (int j = 0; j < GetHiddenWordLenght; j++)
+                {
+                    if (_hiddenWord[i] == guess[j])
+                    {
+                        if (i == j)
+                        {
+                            _bulls++;
+                        }
+                        else
+                        {
+                            _cows++;
+                        }
+                    }
+                }
+            }
+
+            if (_bulls == GetHiddenWordLenght)
+            {
+                _isWon = true;
+            }
+
+            _currentTry++;
+        }
+    }
+}
---
+++ Cours/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace Cours
+{
+    internal class Program
+    {
+        private const string Version = "0.1.0";
+
+        static readonly BullsCowsGame BcGame = new BullsCowsGame();
+
+        // Entry Point
+        public static void Main(string[] args)
+        {
+            bool bPlayAgain;
+
+            PrintIntro();
+
+            do
+            {
+                PlayGame();
+                bPlayAgain = AskToPlayAgain();
+            } while (bPlayAgain);
+        }
+
+        // Print Game Title and Intro.
+        static void PrintIntro()
+        {
+            Console.WriteLine("Welcome to Bulls and Cows");
+            Console.WriteLine("                            ");
+            Console.WriteLine("          }   {         ___ ");
+            Console.WriteLine("          (o o)        (o o) ");
+            Console.WriteLine("   /-------\\ /          \\ /-------\\ ");
+            Console.WriteLine("  / | BULL |O            O| COW  | \\ ");
+            Console.WriteLine(" *  |-,--- |              |------|  * ");
+            Console.WriteLine("    ^      ^              ^      ^ ");
+            Console.WriteLine("C# version: " + Version);
+            Console.WriteLine("");
+        }
+
+        // Main Loop of the game.
+        static void PlayGame()
+        {
+            Console.WriteLine("Can you guess the "+ BcGame.GetHiddenWordLenght + " letters isogram I'm thinking of?");
+            while (BcGame.GetCurrentTry <= BcGame.GetMaxTries && !BcGame.IsGameWon)
+            {
+                BcGame.SubmitGuess(GetGuess());
+                Console.WriteLine("Bulls: " + BcGame.GetBullsCount + " - Cows: " + BcGame.GetConwsCount);
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine(BcGame.IsGameWon
+                ? "Congratulation, you won!"
+                : "Bad luck, maybe you'll do better next time.");
+        }
+
+        //Ask the player if he wants to play again.
+        private static bool AskToPlayAgain()
+        {
+            Console.WriteLine("Do you want to play again? (y/n)");
+            string playAgain = Console.ReadLine();
+            if (playAgain != null)
+            {
+                Console.WriteLine(playAgain[0]);
+
+                if (playAgain[0] == 'y')
+                {
+                    BcGame.Reset();
+                    return true;
+                }
+            }
+
+            Console.WriteLine("Goodbye!");
+            return false;
+        }
+
+
+        // Ask the player for a Guess and validate it.
+        private static string GetGuess()
+        {
+            string guess;
+            BullsCowsGame.EGuessStatus valid;
+
+            do
+            {
+                Console.WriteLine("Try " + BcGame.GetCurrentTry + " of " + BcGame.GetMaxTries + " - Please enter your guess:");
+                guess = Console.ReadLine();
+
+                valid = BcGame.ValidateGuess(guess);
+
+                switch (valid)
+                {
+                    case BullsCowsGame.EGuessStatus.WrongLengh:
+                        Console.WriteLine("Please enter a word that is " + BcGame.GetHiddenWordLenght + " letters long.");
+                        break;
+
+                    case BullsCowsGame.EGuessStatus.NotLowerCase:
+                        Console.WriteLine("Please only use lowercase.");
+                        break;
+
+                    case BullsCowsGame.EGuessStatus.NotIsogram:
+                        Console.WriteLine("Please enter an isogram.");
+                        break;
+                }
+            } while (valid != BullsCowsGame.EGuessStatus.Ok);
+
+            return guess;
+        }
+    }
+}
